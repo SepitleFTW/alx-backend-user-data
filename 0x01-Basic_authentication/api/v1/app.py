ebbd0876696c0f9f4d51f14af1b44b218adc6b73
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-08-06 20:31:45 +0200

    ffixed too many blank lines

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -22,7 +22,6 @@ def not_found(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
