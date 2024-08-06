bb4e2ad9e21896c3b6e8fcea203ee2852a7c7eeb
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-08-06 21:41:24 +0200

    ...

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,32 +13,15 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+if getenv('AUTH_TYPE') == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
-
-@app.errorhandler(401)
-def unauthorized_request(error) -> str:
-    """
-    unauthroized handling requests
-    returns error code 401
-    """
-
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden_request(error) -> str:
-    """
-    fucntion for error because of forbidden request
-    returns error code 403
-    """
-
-    return jsonify({"error": "Forbidden"}), 403
+elif getenv('AUTH_TYPE') == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
@@ -59,6 +42,33 @@ def filter_request():
 
     if auth.current_user(request) is None:
         abort(403)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized_request(error) -> str:
+    '''Unauthorized requests
+    '''
+
+    return jsonify({
+        "error": "Unauthorized"
+    }), 401
+
+
+@app.errorhandler(403)
+def forbidden_request(error) -> str:
+    '''Forbidden request
+    '''
+
+    return jsonify({
+        "error": "Forbidden"
+    }), 403
 
 
 if __name__ == "__main__":
