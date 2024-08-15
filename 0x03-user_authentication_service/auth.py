5d54b51647237415e723693ab6c77178acd964a7
SepitleFTW <136067402+SepitleFTW@users.noreply.github.com>
2024-08-15 19:04:59 +0200

    implemented uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,6 +14,13 @@ def _hash_password(password: str) -> bytes:
     password hashing method
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    generate a uuid
+    """
+    return str(uuid4())
 
 
 class Auth:
